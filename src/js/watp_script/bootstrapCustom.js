2b2405ee5d530439647bbae74e7403982799f49f
jhotadhari <tellme@waterproof-webdesign.info>
2020-04-30 09:34:16 +0530

    Strip Bootstrap scss and js

--- src/js/watp_script/bootstrapCustom.js
+++ src/js/watp_script/bootstrapCustom.js
@@ -12,18 +12,18 @@ import $ from 'jquery'
 /**
  * Bootstrap dependencies
  */
-import Alert from 'bootstrap/js/src/alert'
-import Button from 'bootstrap/js/src/button'
-import Carousel from 'bootstrap/js/src/carousel'
+// import Alert from 'bootstrap/js/src/alert'
+// import Button from 'bootstrap/js/src/button'
+// import Carousel from 'bootstrap/js/src/carousel'
 import Collapse from 'bootstrap/js/src/collapse'
-import Dropdown from 'bootstrap/js/src/dropdown'
-import Modal from 'bootstrap/js/src/modal'
-import Popover from 'bootstrap/js/src/popover'
-import Scrollspy from 'bootstrap/js/src/scrollspy'
-import Tab from 'bootstrap/js/src/tab'
-import Toast from 'bootstrap/js/src/toast'
-import Tooltip from 'bootstrap/js/src/tooltip'
-import Util from 'bootstrap/js/src/util'
+// import Dropdown from 'bootstrap/js/src/dropdown'
+// import Modal from 'bootstrap/js/src/modal'
+// import Popover from 'bootstrap/js/src/popover'
+// import Scrollspy from 'bootstrap/js/src/scrollspy'
+// import Tab from 'bootstrap/js/src/tab'
+// import Toast from 'bootstrap/js/src/toast'
+// import Tooltip from 'bootstrap/js/src/tooltip'
+// import Util from 'bootstrap/js/src/util'
 
 /**
  * --------------------------------------------------------------------------
@@ -50,16 +50,16 @@ import Util from 'bootstrap/js/src/util'
 })()
 
 export {
-  Util,
-  Alert,
-  Button,
-  Carousel,
+  // Util,
+  // Alert,
+  // Button,
+  // Carousel,
   Collapse,
-  Dropdown,
-  Modal,
-  Popover,
-  Scrollspy,
-  Tab,
-  Toast,
-  Tooltip
+  // Dropdown,
+  // Modal,
+  // Popover,
+  // Scrollspy,
+  // Tab,
+  // Toast,
+  // Tooltip
 }
